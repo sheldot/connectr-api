91aebeac491f6f2118addc48a5be67fd24b08916
Sheldon Trotman <sheldot@users.noreply.github.com>
2023-07-22 13:48:46 +0200

    update to the routes

--- src/api/apiRouter.ts
+++ src/api/apiRouter.ts
@@ -3,8 +3,14 @@ import { Router } from "express";
 import apiRouter from "./routes/api.routes";
 import apiFieldsRouter from "./routes/apiFields.routes";
 
+const ping = async (_: any, res: any) => {
+  res.status(200).send("pong");
+};
+
 const coreRouter = () => {
   const appRouter = Router();
+
+  appRouter.get("/ping", ping);
 
   appRouter.use("/api/:apiId/fields", apiFieldsRouter);
   appRouter.use("/api", apiRouter);
--- src/api/routes/apiFields.routes.ts
+++ src/api/routes/apiFields.routes.ts
@@ -4,11 +4,6 @@ import getApis from "../controllers/api/getApis";
 
 const apiRouter = Router();
 
-const ping = async (_: any, res: any) => {
-  res.status(200).send("pong");
-};
-
-apiRouter.get("/ping", ping);
 apiRouter.get("/", getApis);
 
 // apiRouter.post("/", createApi);
